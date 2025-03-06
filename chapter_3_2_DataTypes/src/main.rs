d50bd5a22afb3c3a0ce47b7ea98d4cece0ec1535
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-03-07 00:19:07 +0530

    some other changes.

--- chapter_3_2_DataTypes/src/main.rs
+++ chapter_3_2_DataTypes/src/main.rs
@@ -21,24 +21,43 @@ fn main() {
      * -----------------------------------
      * \           \          \         \\
      * \  length   \  signed  \ Unsined \\
-     * \           \          \         \\
+     * \           \   +/-    \    +    \\
      * -----------------------------------
      * \\  8-bit   \   i8     \   u8    \\
      * \\  16-bit  \   i16    \   u16   \\
      * \\  32-bit  \   i32    \   u32   \\
      * \\  64-bit  \   i64    \   u64   \\
      * \\  128-bit \   i128   \   u128  \\
-     * \\   ARCH   \   i16    \   u16   \\
+     * \\----------\--------------------\\
+     * \\   ARCH   \architecture depend \\
      * ===================================
      *
      * */
 
-    let a = 98_324;
-    let b = 0xff;
-    let c = 0o77;
-    let d = 0b111_0000;
-    let e = b'A';
-    let f = 255;
+    // B: ARCHITECTURE = mostyle (32/64)
 
-    println!("{ a }", "{ b }", "{ c }", "{ d }", " { e }", "{ f }");
+    let a = 98_324; // Decimal
+    let b = 0xfffa; // Hex
+    let c = 0o7735; // Octal
+    let d = 0b1111; // Binery
+    let e = b'A'; // Bite U8 only.
+    let f: u8 = 255; // G: intiger oferflow 👇
+
+    /* B: Intiger Overflow:
+     *  \--------------------------------------------------------\
+     *  \f:u8 = 255 is ( 8 bit unsigned intiger) maxvalue = 255  \
+     *  \--------------------------------------------------------\
+     *  \                                                        \
+     *  \       assigingin it higher value:                      \
+     *  \    A. while DEBUG: rust will panic---                  \
+     *  \    B. while RELEASE: rust will do wrapping....         \
+     *  \        if biger then   255 then                        \
+     *  \                                                        \
+     *  \--------------------------------------------------------\
+     * */
+
+    let g = 2.0; //  G: float default f64
+    let h: f32 = 3.0; //  forcefully assgined f32
+
+    // println!("{ a }", "{ b }", "{ c }", "{ d }", " { e }", "{ f }");  // dont know if it will run or not.
 }
