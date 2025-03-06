f38ae5b4233e02a3022dacf1dd917e56ea3b3e2a
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-03-06 23:57:02 +0530

    updates on hightime....

--- chapter_3_2_DataTypes/src/main.rs
+++ chapter_3_2_DataTypes/src/main.rs
@@ -2,4 +2,43 @@ const TWO: u32 = 1 + 1;
 
 fn main() {
     println!("{TWO}");
+    /* Y: 2 main data types:
+     * A. Scaller Datatypes.
+     * B. Compound Datatypes.
+     */
+
+    /* G: A.Scaller
+     *  ---------------
+     *  Intiger
+     *  Floatpoint number
+     *  Boolean
+     *  Charecter
+     *
+     */
+
+    /* B:
+     * INT:
+     * -----------------------------------
+     * \           \          \         \\
+     * \  length   \  signed  \ Unsined \\
+     * \           \          \         \\
+     * -----------------------------------
+     * \\  8-bit   \   i8     \   u8    \\
+     * \\  16-bit  \   i16    \   u16   \\
+     * \\  32-bit  \   i32    \   u32   \\
+     * \\  64-bit  \   i64    \   u64   \\
+     * \\  128-bit \   i128   \   u128  \\
+     * \\   ARCH   \   i16    \   u16   \\
+     * ===================================
+     *
+     * */
+
+    let a = 98_324;
+    let b = 0xff;
+    let c = 0o77;
+    let d = 0b111_0000;
+    let e = b'A';
+    let f = 255;
+
+    println!("{ a }", "{ b }", "{ c }", "{ d }", " { e }", "{ f }");
 }
