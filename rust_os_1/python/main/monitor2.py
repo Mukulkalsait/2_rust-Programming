2ba281aa66a9d782a21cc2043b2cf0c81ede967b
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-26 23:57:38 +0530

    Updated plan 1

---
+++ rust_os_1/python/main/monitor2.py
@@ -0,0 +1,67 @@
+# B: Monitor.py
+# * Process detection * Monitoring * Restart logic * Matching processes started by forever * Avoid duplicate restarts * Keep running continuously
+#
+# G: Program 2 — monitor
+# Takes one argument — the name of the program it should watch | Continuously checks if any process started by forever dies | 
+# If any dies, automatically restarts it with the same command | Should keep running and monitoring
+#
+# B: We need:
+# A way to identify processes started by forever | A way to know if they die
+# A way to know how to restart them | A way to avoid duplicate restarts
+# Continuous monitoring loop
+
+
+import os
+import sys
+import time 
+import subprocess
+
+REG_FILE = "registry.txt"
+INTERVAL = 3
+
+def read_registry():
+    if not os.path.exists(REG_FILE):
+        return None, None
+    with open(REG_FILE,"r") as f:
+        line = f.readline().strip()
+    if not line:
+        return None, None
+
+    parts = line.split()
+    pid = int(parts[0])
+    cmd = parts[1:]
+    return pid , cmd
+
+def process_alive(pid):
+    try:
+        os.kill(pid, 0)
+        return True
+    except ProcessLookupError:
+        return False
+    except PermissionError:
+        return True
+
+def restart_process(cmd):
+    print(f"Restartingt Process: '{' '.join(cmd)}'")
+    process = subprocess.Popen(cmd)
+    with open(REG_FILE, "w") as f:
+        f.write(f"{process.pid} {' '.join(cmd)}\n")
+    return process.pid
+
+def main():
+    print("Monitoring Started .....")
+
+    while True:
+        pid, cmd = read_registry()
+
+        if pid is None:
+            time.sleep(INTERVAL)
+            continue
+        if not process_alive(pid):
+            print(f"Process {pid} is dead.")
+            restart_process(cmd)
+
+        time.sleep(INTERVAL)
+
+if __name__ == "__main__":
+    main()
