ee19f2c7a3f1a57f88894033fe63c1285c68bce4
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-03 05:30:13 +0530

    reqwest updated

--- chapter_5_reqwest/src/models.rs
+++ chapter_5_reqwest/src/models.rs
@@ -1,13 +1,35 @@
 use serde::{Deserialize, Serialize};
 
-// REQUESTS --------------------------------------------------------------------------------------------------------------------
+// Y: REQUESTS --------------------------------------------------------------------------------------------------------------------
+// here we sent request.
+// THEREFORE:
+//  1. data from struct => json [SERIALIZATION]
+//
+//  SerializationList
+//  1.LoginRequest
+
 #[derive(Serialize, Debug)]
 pub struct LoginRequest {
     pub email: String,
     pub passwd: String,
 }
 
-// RESPONSES -------------------------------------------------------------------------------------------------------------------
+#[derive(Serialize, Debug)]
+pub struct SearchQuery {
+    pub q: String,
+    pub page: u16,
+}
+
+// R: RESPONSES -------------------------------------------------------------------------------------------------------------------
+// here we reciece the data.
+//  THEREFORE:
+//  1. json => Struct [DESERALIZATION1]
+//
+//  DeserializationList
+//  1. LoginResponse ->Option<String>
+//  2. SearchResponse ->Vec<String>
+//  3. UploadResponse ->String
+
 #[derive(Deserialize, Debug)]
 pub struct LoginResponse {
     pub token: Option<String>,
@@ -16,6 +38,7 @@ pub struct LoginResponse {
 #[derive(Deserialize, Debug)]
 pub struct SearchResponse {
     pub results: Vec<String>,
+    // Y:  vec because we don tknow how many resualts we might gee.
 }
 
 #[derive(Deserialize, Debug)]
