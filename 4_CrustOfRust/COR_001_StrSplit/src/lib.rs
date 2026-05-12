17ab5dbc9b523c56d6fe4f8b020ad3e4ba07358a
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-13 05:08:50 +0530

    Updated

--- 4_CrustOfRust/COR_001_StrSplit/src/lib.rs
+++ 4_CrustOfRust/COR_001_StrSplit/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod delimiter;
+mod strsplit;
+mod tests;
+mod utils;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+use crate::strsplit::StrSplit;
