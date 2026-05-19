5a0526a04573709c7b6cbabf625e53d40d83ef6f
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-20 03:58:18 +0530

    Self learned Proper error handling inrust with Resualt<T,Err> type, and match/unwrap_or_else methods

---
+++ chapter_02_Basics-2/ch_03-X2/target/debug/deps/chapter_3_X2-a634508b332443dd.d
@@ -0,0 +1,9 @@
+/home/mukuldk/1Home/1Projects/class/2_rust-Programming/chapter_3-X2/target/debug/deps/libchapter_3_X2-a634508b332443dd.rmeta: src/main.rs Cargo.toml
+
+/home/mukuldk/1Home/1Projects/class/2_rust-Programming/chapter_3-X2/target/debug/deps/chapter_3_X2-a634508b332443dd.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
