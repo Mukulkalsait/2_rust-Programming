633ee54505273fe2d096e32a04128b67a618abb4
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-29 01:11:13 +0530

    User modle design keeping in migrations in mind

---
+++ chapter_12_backend_full/src/db/users/user_modle.rs
@@ -0,0 +1,34 @@
+// FILE: /src/data_modles/users.rs
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, Clone, sqlx::Type, PartialEq)] // add =>  partialEq
+#[sqlx(type_name = "user_role", rename_all = "lowercase")]
+pub enum UserRole {
+    Admin,
+    User,
+}
+
+impl UserRole {
+    pub fn to_string(&self) -> &str {
+        match self {
+            UserRole::Admin => "admin",
+            UserRole::User => "user",
+        }
+    }
+}
+
+#[derive(Debug, sqlx::FromRow, sqlx::Type, Clone)]
+pub struct User {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub email: String,
+    pub password: String,
+    pub role: UserRole,
+    pub verified: bool,
+    pub verification_token: Option<String>,
+    pub token_expire_at: Option<DateTime<Utc>>,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
+}
