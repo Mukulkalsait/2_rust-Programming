3c98a7c9062114e9ecec7a882c4378f7fedf54d0
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-29 01:11:27 +0530

    Main funciton currently with todo!() macro

--- chapter_12_backend_full/src/main.rs
+++ chapter_12_backend_full/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+// ─── Local Modules ────────────────────────────────────────────────────
+mod config;
+mod db;
+// ─── END: =============================================================
+
+fn main() -> anyhow::Result<()> {
+    let app_env_config = config::env::AppConfigEnv::from_env()?;
+
+    todo!();
 }
