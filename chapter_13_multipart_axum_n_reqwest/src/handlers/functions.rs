421af2968d584497d585b6617a388aaa0e9abe25
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-07-06 07:36:08 +0530

    Updates

--- chapter_13_multipart_axum_n_reqwest/src/handlers/functions.rs
+++ chapter_13_multipart_axum_n_reqwest/src/handlers/functions.rs
@@ -1,91 +1,187 @@
+use std::collections::HashMap;
 
-use axum::{self, extract::{Path}, http::StatusCode};
-use crate::handlers::helper_functions::{ delete_file_on_default_upload_directory, get_upload_dir, save_file_on_default_upload_directory};
+use crate::handlers::helper_functions::{get_upload_dir, save_file_on_default_upload_directory};
+use axum::{
+    self,
+    Json,
+    extract::{Multipart, Path, Query},
+    http::StatusCode,
+    response::ErrorResponse,
+};
 
-
-
+// Y: ============================================================================ Basic fn
+//
 /// just working or not.
-pub async fn workign()-> String{ "✅ WORKING".to_string() }
-
+pub async fn workign() -> String { "✅ WORKING".to_string() }
 /// just working or not for homepage.
-pub async fn home_page()-> String{ "󰠧 HOME ".to_string() }
+pub async fn home_page() -> String { "󰠧 HOME ".to_string() }
+
+// Y: ============================================================================ INFO GETING FUNCTIONF FROM AXUM
+
+/// GET query parameters
+pub async fn transfer_search_handler(Query(params): Query<HashMap<String, String>>) {
+    let query = params.get("q");
+    println!("query = {:?}", query)
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct TempUserData {}
+
+#[axum::debug_handler]
+pub async fn transfer_create_data(Json(payload): Json<TempUserData>) {
+    println!("{:?}", payload);
+}
+
+// pub async fn multipart_transfer_handler(axum::extract::Multipart(multipart) :  ){
+//     println!("{}",multipart)
+// }
+
+// Y: ============================================================================ TEST functions
 
 /// ### Create dummy file and test the function:
 /// ***function::save_file_on_default_upload_directory()***
-pub async fn test_dummy_file_save(){
+pub async fn test_dummy_file_saveing() {
     let test_bytes = b"This is test files".to_vec(); // test
     let test_filename = "test.md".to_string(); // taken as test because you did not told me where ot get this part? 
-    
-    match save_file_on_default_upload_directory(test_bytes, &test_filename).await{
-        Ok(saved_name) => println!("✅ File Saved: {}",saved_name),
-        Err(e) => println!("❌ Faild to save file: {}",e),
+
+    match save_file_on_default_upload_directory(test_bytes, &test_filename).await {
+        Ok(saved_name) => println!("✅ File Saved: {}", saved_name),
+        Err(e) => println!("❌ Faild to save file: {}", e),
     }
 }
 
+// Y: ============================================================================ MAIN FUNCTIONS
 #[axum::debug_handler]
 /// ## to verify the file exists or not on server uploades folder.
 /// ``` rust
-///  // getting 
+///  // getting
 ///  Axum::extract::Path =  Path<String>
 ///  // create full path with upload dir + path with format!
-/// std::fs::read(complete_file_path) // check if its available or not => 
+/// std::fs::read(complete_file_path) // check if its available or not =>
 /// ```
 ///  ---
-pub async fn check_if_post_file_present_or_not( axum::extract::Path(filename): Path<String>)-> Result<Vec<u8>,axum::http::StatusCode>{
-    let complete_file_path =  format!("{}/{}",get_upload_dir(),filename);
+pub async fn check_file_present_or_not(axum::extract::Path(filename): Path<String>) -> Result<Vec<u8>, axum::http::StatusCode> {
+    let complete_file_path = format!("{}/{}", get_upload_dir(), filename);
     std::fs::read(complete_file_path).map_err(|_| StatusCode::NOT_FOUND)
 }
 
+///
+/// use Multipart => while let some(field / mut field) = multipart.next_field().await.unwrap{...}
+/// provides the filed.{headers(), name(), fille_name() bites() }
+///
+pub async fn post_save_file(mut multipart: axum::extract::Multipart) -> impl axum::response::IntoResponse {
+    let mut res = Vec::new();
 
-pub async fn post_upload_handler(mut multipart: axum::extract::Multipart)-> String{
-
-    while let Some(field) =  multipart.next_field().await.unwrap(){
-        println!("headers: {:?}",field.headers());
-        println!("content_type: {}",field.content_type().unwrap_or(""));
-        println!("name: {}",field.name().unwrap_or(""));
-        println!("filename: {}",field.file_name().unwrap_or(""));
-
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let filed_headers = field.headers();
+        let field_content_types = field.content_type().unwrap_or("");
         let field_name = field.name().unwrap_or("").to_string();
-        println!("field_name: {}",field_name);
         let file_name = field.file_name().unwrap_or("").to_string();
+
+        // B: The vars above live only till this steatement lives, thats why if we put let bytes above this statement it will throgh error all because of  ".await"
+        println!(" headers:{:?}\n content_type:{} \n name:{} \n filename:{}", filed_headers, field_content_types, field_name, file_name);
+
         let bytes = field.bytes().await.unwrap().to_vec();
 
-        match save_file_on_default_upload_directory(bytes, &file_name).await{
-            Ok(m) =>  {println!("✅ FILE Saved: {}",m)},
-            Err(e) => {println!("❌ Faild to save file: {}", e)}
+        match save_file_on_default_upload_directory(bytes, &file_name).await {
+            Ok(filename) => res.push(format!("✅ FILE Saved: {}", filename)),
+            Err(e) => {
+                res.push(format!("❌ Faild to save file: {}", e));
+            }
         }
-    }   
-    "upload endpoint hit".to_string()
-}
-
-
-// pub async fn get_file_handler(Path(filename):Path<String>)->String{
-//     match read_file(filename).await{
-//         Ok(bites)=>{
-//             match String::from_utf8(bites){
-//                 Ok(content) => content,
-//                 Err(_) => "Binery file not found to display as text".to_string(),
-//             }
-//             Err(e) =>format!("Error reading file: {}",e),
-//
-//         },
-//
-//     }
-//
-// }
-//
-
-pub async fn post_delete_file(mut multipart: axum::extract::Multipart)-> StatusCode{
-// delete_file_on_default_upload_directory(&filename)
-
-    let mut filename :&str;
-    while let Some(field) = multipart.next_field().await.unwrap() {
-            let filedname= field.name().unwrap_or("");
-            let filename = field.file_name().unwrap_or("");
-            
     }
-
-    if let Some(res) = delete_file_on_default_upload_directory(filename){
-        StatusCode::OK
-    } else { StatusCode::INTERNAL_SERVER_ERROR }
+    (StatusCode::OK, res.join("\n"))
 }
+
+pub async fn delete_file(axum::extract::Path(filename): Path<String>) -> Result<(), StatusCode> {
+    // delete_file_on_default_upload_directory(&filename)
+    let complete_file_path = format!("{}/{}", get_upload_dir(), filename);
+
+    std::fs::remove_file(complete_file_path).map_err(|e| match e.kind() {
+        std::io::ErrorKind::NotFound => StatusCode::NOT_FOUND,
+        std::io::ErrorKind::PermissionDenied => StatusCode::FORBIDDEN,
+        _ => StatusCode::INTERNAL_SERVER_ERROR,
+    })
+}
+
+pub async fn delete_best_implimentation(axum::extract::Path(filename): Path<String>) -> Result<(), StatusCode> {
+    let path: std::path::PathBuf = [get_upload_dir(), filename].iter().collect();
+
+    tokio::fs::remove_file(&path).await.map_err(|e| {
+        // Y: Tracing
+        // tracing::error!("Faild to delete {}:{}",filename, e);
+        match e.kind() {
+            std::io::ErrorKind::NotFound => StatusCode::NOT_FOUND,
+            std::io::ErrorKind::PermissionDenied => StatusCode::FORBIDDEN,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    })
+}
+
+struct FileInfo {
+    name: String,
+    size: u64,
+    modified: String,
+    path: String,
+}
+
+struct FileListResponse {
+    files: Vec<FileInfo>,
+    count: usize,
+    total_size: u64,
+}
+
+pub async fn list_downloads() {
+    let path = get_upload_dir();
+    let files = std::fs::read_dir(path);
+
+    let entries = match files {
+        Ok(read_dir) => read_dir,
+        Err(e) => {
+            return match Some(e) = e.kind() {
+                std::io::ErrorKind::NotFound => StatusCode::NOT_FOUND,
+                std::io::ErrorKind::PermissionDenied => StatusCode::FORBIDDEN,
+                _ => StatusCode::INTERNAL_SERVER_ERROR,
+            };
+        }
+    };
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
