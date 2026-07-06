1e2a7fb75f4814d2b164712c204635752b5d183f
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-07-06 07:36:09 +0530

    Updates

--- chapter_13_multipart_axum_n_reqwest/src/handlers/helper_functions.rs
+++ chapter_13_multipart_axum_n_reqwest/src/handlers/helper_functions.rs
@@ -1,41 +1,46 @@
-use axum::{extract::Path, http::StatusCode};
-
-
-
-
-
-/// providing default upload directory into root/upload 
+/// providing default upload directory into root/upload
 ///  => if present return , else create and return
-pub fn get_upload_dir()->String {
+pub fn get_upload_dir() -> String {
     if !std::path::Path::new("./uploads").exists() {
         std::fs::create_dir_all("./uploads").unwrap()
     }
     "./uploads".to_string()
 }
 
+/// ### PATH => convert name into Path like Object where it konws what is "/" and "." and extensions, everting.
+///
+/// > it provides :
+/// ```
+/// exists() - Check if file/directory exists
+/// is_file() / is_dir() - Check what it is
+/// file_name() - Get filename from path
+/// extension() - Get file extension
+/// parent() - Get parent directory
+/// join() - Join paths together (handles / vs \ automatically)
+/// to_str() - Convert to string
+///
+/// ```
+pub async fn save_file_on_default_upload_directory(bytes: Vec<u8>, orignal_filename: &str) -> Result<String, std::io::Error> {
+    let path_obj = std::path::Path::new(orignal_filename);
+    let extension_extration = path_obj.extension().and_then(|extension| extension.to_str()).unwrap_or("bin").to_string();
 
-pub async fn save_file_on_default_upload_directory(bytes: Vec<u8>,orignal_filename: &str)->Result<String, std::io::Error>{
+    println!(
+        "Filename:{:?} \nIs empty ?:{} \nAbsolute path: {:?} \nIs Directory: {} \nIs File: {} \nInside ***save_file_on_default_upload_directory*** \norignal_filename: {}\n file_entenxion_extrated_from_path: {:?}\n extension_extration: {}",
+        path_obj.file_name(),
+        path_obj.is_empty(),
+        path_obj.is_absolute(),
+        path_obj.is_dir(),
+        path_obj.is_file(),
+        orignal_filename,
+        path_obj,
+        extension_extration
+    );
 
-    // geting name of file with path
-    let file_entenxion_extrated_from_path = std::path::Path::new(orignal_filename);
-    // extension processing
-    let extension_extration = file_entenxion_extrated_from_path.extension().and_then(|extension| extension.to_str()).unwrap_or("bin").to_string();
-
-    println!( "Inside ***save_file_on_default_upload_directory*** \norignal_filename: {}\n file_entenxion_extrated_from_path: {:?}\n extension_extration: {} ",orignal_filename,file_entenxion_extrated_from_path, extension_extration);
     // R: debuging
 
-    let uploading_filename = format!("file_{}.{}", chrono::Utc::now().timestamp(),extension_extration); // return this to user...
-    let path = format!("{}/{}",get_upload_dir(),uploading_filename);
+    let uploading_filename = format!("file_{}.{}", chrono::Utc::now().timestamp(), extension_extration); // return this to user...
+    let save_path = format!("{}/{}", get_upload_dir(), uploading_filename);
 
-    std::fs::write(&path, bytes)?;
-
+    std::fs::write(&save_path, bytes)?;
     Ok(uploading_filename)
-}
-
-
-
-pub async fn delete_file_on_default_upload_directory(orignal_file_name: &str)-> Result<&str,std::io::Error>{
-    let full_path = format!("{}/{}",get_upload_dir(), orignal_file_name);
-    println!("Deleting: {}",full_path);
-    if std::fs::exists(&full_path).unwrap(){ std::fs::remove_dir(full_path).and_then(Ok(())) }else{ Ok(&full_path)}
 }
