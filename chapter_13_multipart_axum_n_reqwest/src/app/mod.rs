b01268a3f97dc37fa45e98632c5e96e4bc18ddbe
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-07-06 07:36:06 +0530

    Updates

--- chapter_13_multipart_axum_n_reqwest/src/app/mod.rs
+++ chapter_13_multipart_axum_n_reqwest/src/app/mod.rs
@@ -1,8 +1,11 @@
-use axum::{routing::get, routing::post, Router};
+use axum::{Router, routing::delete, routing::get, routing::post};
 use tower_http::cors::{Any, CorsLayer};
 
 // EXT
-use crate::handlers::*;
+use crate::handlers::{
+    functions::{transfer_create_data, transfer_search_handler},
+    *,
+};
 
 /// > This function is not await.
 /// > unless we are actually awating someting inside it like db connection and all  
@@ -11,10 +14,21 @@ pub fn build_app() -> axum::Router {
     let cores = CorsLayer::new().allow_origin(Any).allow_headers(Any).allow_methods(Any);
 
     Router::new()
+        //
+        // Y: Test Functions
         .route("/", get(functions::workign))
         .route("/home", get(functions::home_page))
-        .route("/test0", get(functions::test_dummy_file_save))
-        .route("/file/{filename}", get(functions::check_if_post_file_present_or_not))
-        .route("/upload", post(functions::post_upload_handler))
+        //
+        // Y: Data Transfering Functions
+        .route("/transfer_search", get(transfer_search_handler))
+        .route("/transfer_data", post(transfer_create_data))
+        // .route("/transfer_upload", post(multipart_transfer_handler))
+        //
+        // Y: Processing Functions
+        .route("/test0", get(functions::test_dummy_file_saveing))
+        .route("/file/{filename}", get(functions::check_file_present_or_not))
+        .route("/upload", post(functions::post_save_file)) // Y:  file should be come inside the request
+        .route("/delete/{filename}", delete(functions::delete_file))
+        .route("/delete/{filename}", delete(functions::delete_best_implimentation))
         .layer(cores)
 }
