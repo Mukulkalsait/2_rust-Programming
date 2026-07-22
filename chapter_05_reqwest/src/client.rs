94672e977fd13a0d883bb726888254f676e096a6
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-07-22 21:33:05 +0530

    Library system architecture building

--- chapter_05_reqwest/src/client.rs
+++ chapter_05_reqwest/src/client.rs
@@ -4,6 +4,7 @@ use reqwest::{
 };
 use std::time::Duration;
 
-pub fn build_client() -> Client {
-    ClientBuilder::new().timeout(Duration::from_secs(5)).redirect(Policy::limited(3)).build().unwrap()
-}
+/// # Reqwest: lib to sent request.
+///   - **[Client:]** Browser like service, with diffault settings
+///   - **[ClientBuilder:]** Costumize it.
+pub fn build_client() -> Client { ClientBuilder::new().timeout(Duration::from_secs(5)).redirect(Policy::limited(3)).build().unwrap() }
